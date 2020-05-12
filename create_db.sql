413f933855dc6c73934b8509fd0330a35ef16657
Patrick McCann <pgm5@st-andrews.ac.uk>
2020-05-12 10:48:03 +0100

    Normalises DB

--- create_db.sql
+++ create_db.sql
@@ -1,7 +1,14 @@
 CREATE TABLE requests (
   id INTEGER PRIMARY KEY,
-  request TEXT NOT NULL,
+  request TEXT NOT NULL
+);
+
+CREATE TABLE responses (
+  id INTEGER PRIMARY KEY,
+  request_id INTEGER NOT NULL,
   success BOOLEAN NOT NULL CHECK (success IN (0,1)),
   response_file TEXT,
-  timestamp DATETIME NOT NULL
+  timestamp DATETIME NOT NULL,
+  FOREIGN KEY(request_id) REFERENCES requests(request)
 );
+
